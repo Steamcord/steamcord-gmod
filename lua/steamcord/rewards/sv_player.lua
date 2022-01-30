4803515692c0d28e1fb5017fa7b7ca7a2a52686f
nosharp <contact@nosharp.cc>
2022-01-30 22:32:38 +0000

    feat: add support for onJoin rewards.

--- lua/steamcord/rewards/sv_player.lua
+++ lua/steamcord/rewards/sv_player.lua
@@ -1,21 +1,27 @@
-function Steamcord.Rewards.CheckRewardsForPlayers(player, callback)
-    local steamId = player:SteamID64()
-    Steamcord.RestAPI.GETPlayer(steamId, function(playerObj)
 
+local redeemedRewards = {}
+
+function Steamcord.Rewards.CheckRewardsForPlayers(player, callback)
+
+    local steamId = player:SteamID64()
+    redeemedRewards[steamId] = redeemedRewards[steamId] or {}
+    Steamcord.RestAPI.GETPlayer(steamId, function(playerObj)
         local requirements = playerObj:GetAchievedRequirements()
         local rewards = Steamcord.Rewards.FindApplicableRewards(requirements)
         
         for _,reward in ipairs(rewards) do
-        
+            if redeemedRewards[steamId][reward:GetName()] then continue end
+            
             if reward:GetIsRunOnce() then
                 Steamcord.Data.HasRedeemed(steamId, reward:GetName(), function(steamId, rewardType, hasRedeemed)
                     if not hasRedeemed then
                         reward:RewardPlayer(player)
+                        redeemedRewards[steamId][reward:GetName()] = true
                     end
-                    
                 end)
             else
                 reward:RewardPlayer(player)
+                redeemedRewards[steamId][reward:GetName()] = true
             end
         
         end
@@ -46,3 +52,7 @@ do
     end)
 
 end
+
+hook.Add("PlayerInitialSpawn", "Steamcord::OnPlayerJoin", function(player, _)
+    Steamcord.Rewards.CheckRewardsForPlayers(player)
+end)
