42ce186342a0672452ac378c19b86b987d50f708
nosharp <contact@nosharp.cc>
2022-01-30 21:45:02 +0000

    fix(rewards): isPlayer check failing due to typo

--- lua/steamcord/rewards/sv_reward_funcs.lua
+++ lua/steamcord/rewards/sv_reward_funcs.lua
@@ -1,52 +1,37 @@
 Steamcord.Rewards.Funcs = {}
 
 function Steamcord.Rewards.Funcs.GiveWeapon(player, weapon)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
-    
+    assert(IsEntity(player) and player:IsPlayer(), "The player is not a player.")
     assert(isstring(weapon), "The weapon is not a string.")
-    assert(weapons.Get(weapon) ~= nil, "The weapon is not a valid weapon class!")
 
     player:Give(weapon)
 end
 
 function Steamcord.Rewards.Funcs.AddMoney(player, amount)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
+    assert(IsEntity(player) and player:IsPlayer(), "The player is not a player.")
     assert(isnumber(amount), "The amount of money to give is not a number.")
     
     player:addMoney(amount)
-end
-
-function Steamcord.Rewards.Funcs.AddMoney(player, amount)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
-    assert(isnumber(amount), "The amount of money to give is not a number.")
-    
-    player:addMoney(amount)
-end
-
-function Steamcord.Rewards.Funcs.AddPS1Points(player, amount)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
-    assert(isnumber(amount), "The amount of money to give is not a number.")
-    
-    player:PS_GivePoints(amount)
 end
 
 function Steamcord.Rewards.Funcs.AddPS2PremiumPoints(player, amount)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
+    assert(IsEntity(player) and player:IsPlayer(), "The player is not a player.")
     assert(isnumber(amount), "The amount of money to give is not a number.")
     
     player:PS2_AddPremiumPoints(amount)
 end
 
 function Steamcord.Rewards.Funcs.AddPS2NormalPoints(player, amount)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
+    assert(IsEntity(player) and player:IsPlayer(), "The player is not a player.")
     assert(isnumber(amount), "The amount of money to give is not a number.")
     
     player:PS2_AddStandardPoints(amount)
 end
 
-function Steamcord.Rewards.Funcs.SetUserGroup(player, amount)
-    assert(IsEntity(player) and player:isPlayer(), "The player is not a player.")
-    assert(isnumber(amount), "The amount of money to give is not a number.")
-    
-    player:SetUserGroup(amount)
+function Steamcord.Rewards.Funcs.SetUserGroup(player, usergroup)
+    assert(IsEntity(player) and player:IsPlayer(), "The player is not a player.")
+    assert(isstring(usergroup), "The usergroup is not a string.")   
+    player:SetUserGroup(usergroup)
 end
+
+print(weapons.GetStored("weapon_357") ~= nil)
