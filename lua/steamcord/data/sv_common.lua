e4b7280fce85c76a5ba81ee171e1e469ddb584cd
nosharp <contact@nosharp.cc>
2022-01-30 20:55:15 +0000

    feat: incorporate decoupled db drivers into common lib

--- lua/steamcord/data/sv_common.lua
+++ lua/steamcord/data/sv_common.lua
@@ -6,9 +6,17 @@ end
 
 -- callback will either be true or false as to whether it's been redeemed.
 function Steamcord.Data.HasRedeemed(steamID, rewardName, callback)
-    
+    if Steamcord.Data.IsUsingMysqloo() then
+        Steamcord.MySQL:HasRedeemed(steamID, rewardName, callback)
+    else
+        Steamcord.SQLite:HasRedeemed(steamID, rewardName, callback)
+    end
 end
 
 function Steamcord.Data.SetRedeemed(steamID, rewardName, callback)
-    
+    if Steamcord.Data.IsUsingMysqloo() then
+        Steamcord.MySQL:SetRedeemed(steamID, rewardName, true, callback)
+    else
+        Steamcord.SQLite:SetRedeemed(steamID, rewardName, true, callback)
+    end
 end
