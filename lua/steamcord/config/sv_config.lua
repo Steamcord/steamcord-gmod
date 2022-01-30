ebd21ad8cd16f96b43b7fdfa8793942f16686cf3
nosharp <contact@nosharp.cc>
2022-01-30 22:57:13 +0000

    feat: add config

---
+++ lua/steamcord/config/sv_config.lua
@@ -0,0 +1,60 @@
+Steamcord.Config = {
+    Api = {
+        Token = "Your TokenHere",
+        BaseUri = "https://api.steamcord.io"
+    },
+    ChatCommands = {
+        "/claim",
+        "/link",
+        "/auth"
+    },
+    ProvisionRwardsOnJoin = true,
+    Rewards = {
+        ["DiscordAccount"] = {
+            onGiven = function(ply)
+                AddMoney(ply, 1000)
+                AddPS2PremiumPoints(ply, 15000)
+                AddPS2NormalPoints(ply, 15000)
+                GiveWeapon(ply, "weapon_357")
+            end,
+            requirements = {
+                Steamcord.RestAPI.Enums.MemberOfSteamGroup
+            },
+            -- Should it be ran when the player joins the server?
+            -- If this is true, run once should be false, as this will
+            -- take precedence.
+            runOnJoin = false,
+            -- Added to the selected database, and never given again until wiped.
+            runOnce = false
+        },
+        ["SteamAccount"] = {
+            onGiven = function(ply)
+
+            end,
+            requirements = {
+                Steamcord.RestAPI.Enums.MemberOfSteamGroup
+            },
+            -- Should it be ran when the player joins the server?
+            -- If this is selected, run once should be false, as this will
+            -- take precedence.
+            runOnJoin = false,
+            -- Given once?
+            runOnce = true
+        }
+    },
+    Data = {
+        -- This can either be MySQL or SQLite.
+        -- If you have a MYSQL database and wish to store the redemption data on there,
+        -- do so with MySQL.
+        DatabaseType = "Mysql",
+        -- You DO NOT NEED TO FILL THIS OUT IF YOU HAVE SELECTED SQLITE
+        ConnectionInfo = {
+            Host = "127.0.0.1",
+            Port = 3306,
+            Username = "root",
+            Password = "password",
+            Database = "rewards"
+        }
+    },
+    UpdateSteamGroups = true
+}
