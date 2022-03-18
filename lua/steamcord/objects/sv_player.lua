26f59838f66ca4f9b2b15bf4e2827cd69b481ec7
nosharp <contact@nosharp.cc>
2022-03-18 15:09:09 +0000

    refactor: use new enum style

--- lua/steamcord/objects/sv_player.lua
+++ lua/steamcord/objects/sv_player.lua
@@ -55,17 +55,17 @@ function SteamcordPlayer:GetAchievedRequirements()
     for _,steamAccountObj in ipairs(self.steamAccounts) do
         
         if steamAccountObj:GetIsSteamGroupMember() then
-            hitRequirements[Steamcord.RestAPI.Enums.Requirements.MemberOfSteamGroup] = true
+            hitRequirements[Steamcord.RestAPI.Enums.Requirements.SteamGroupMember] = true
         end
     end
 
     for _,discordAccountObj in pairs(self.discordAccounts) do
         if discordAccountObj:GetIsGuildMember() then
-            hitRequirements[Steamcord.RestAPI.Enums.Requirements.MemberOfDiscordGuild] = true
+            hitRequirements[Steamcord.RestAPI.Enums.Requirements.DiscordGuildMember] = true
         end
 
         if discordAccountObj:GetIsGuildBooster() then
-            hitRequirements[Steamcord.RestAPI.Enums.Requirements.NitroBooster] = true
+            hitRequirements[Steamcord.RestAPI.Enums.Requirements.DiscordGuildBooster] = true
         end
     end
 
